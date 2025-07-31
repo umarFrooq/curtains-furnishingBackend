6a335cee570fe1a438cb50fe0602058ac3ae9b03
umarFarooq <116794065+umarFarooq920@users.noreply.github.com>
2025-07-31 20:06:10 +0500

    up

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ if (process.env.NODE_ENV === 'production' && allowedOrigins.length > 0) {
 
 // Basic route
 app.get('/', (req, res) => {
-  res.send('API is running  you are okssddssssssssss...');
+  res.send('API is running  you are okssddssssdddssssss...');
 });
 
 // Mount routers
